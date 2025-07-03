3b1628aec49abdb6f937040771249d1efab0f062
Xqy <2768851414@qq.com>
2025-07-03 18:55:29 +0800

    mdns模块

---
+++ include/dns.h
@@ -0,0 +1,128 @@
+﻿#ifndef DNS_H
+#define DNS_H
+#include <QtCore>
+#include "hostaddress.h"
+QTNETWORKNG_NAMESPACE_BEGIN
+class BitMapPrivate;
+class BitMap {
+public:
+	BitMap();
+	BitMap(const BitMap& other);
+	BitMap& operator=(const BitMap& other);
+	bool operator==(const BitMap& other);
+	virtual ~BitMap();
+	quint8 length() const;
+	const quint8* data() const;
+	void setData(quint8 length, const quint8* data);
+private:
+	BitMapPrivate* d;
+};
+
+class QueryPrivate;
+class Query {
+public:
+	Query();
+	Query(const Query& other);
+	Query& operator =(const Query& other);
+	virtual ~Query();
+	QByteArray name() const;
+	void setName(const QByteArray& name);
+	quint16 type() const;
+	void setType(quint16 type);
+	bool uincastRespone() const;
+	void setUnicastRespone(bool unicastResponse);
+private:
+	QueryPrivate* const d;
+};
+class RecordPrivate;
+class  Record
+{
+public:
+	Record();
+	Record(const Record& other);
+	Record& operator=(const Record& other);
+	bool operator==(const Record& other) const;
+	bool operator!=(const Record& other) const;
+	virtual ~Record();
+	QByteArray name() const;
+	void setName(const QByteArray& name);
+	quint16 type() const;
+	void setType(quint16 type);
+	bool flushCache() const;
+	void setFlushCache(bool flushCache);
+	quint32 ttl() const;
+	void setTtl(quint32 ttl);
+	HostAddress address() const;
+	void setAddress(const HostAddress& address);
+	QByteArray target() const;
+	void setTarget(const QByteArray& target);
+	QByteArray nextDomainName() const;
+	void setNextDomainName(const QByteArray& nextDomainName);
+	quint16 priority() const;
+	void setPriority(quint16 priority);
+	quint16 weight() const;
+	void setWeight(quint16 weight);
+	quint16 port() const;
+	void setPort(quint16 port);
+	QMap<QByteArray, QByteArray> attributes() const;
+	void setAttributes(const QMap<QByteArray, QByteArray>& attributes);
+	void addAttribute(const QByteArray& key, const QByteArray& value);
+	BitMap bitmap() const;
+	void setBitmap(const BitMap& bitmap);
+private:
+	RecordPrivate* const d;
+};
+class MessagePrivate;
+class Message {
+public:
+	Message();
+	Message(const Message& other);
+	Message& operator=(const Message& other);
+        virtual ~Message();
+        HostAddress address() const;
+        void setAddress(const HostAddress& address);
+	quint16 port() const;
+	void setPort(quint16 port);
+	quint16 transactionId() const;
+	void setTransactionId(quint16 transactionId);
+	bool isResponse() const;
+	void setResponse(bool isResponse);
+	bool isTruncated() const;
+	void setTruncated(bool isTruncated);
+	QList<Query> queries() const;
+	void addQuery(const Query& query);
+	QList<Record> records() const;
+	void addRecord(const Record& record);
+	void reply(const Message& other);
+
+private:
+
+	MessagePrivate* const d;
+};
+enum {
+	/// IPv4 address record
+	A = 1,
+	/// IPv6 address record
+	AAAA = 28,
+	/// Wildcard for cache lookups
+	ANY = 255,
+	/// List of records
+	NSEC = 47,
+	/// Pointer to hostname
+	PTR = 12,
+	/// %Service information
+	SRV = 33,
+	/// Arbitrary metadata
+	TXT = 16
+};
+QDebug operator<<(QDebug dbg, const Query& query);
+QDebug operator<<(QDebug dbg, const Record& record);
+bool parseName(const QByteArray& packet, quint16& offset, QByteArray& name);
+void writeName(QByteArray& packet, quint16& offset, const QByteArray& name, QMap<QByteArray, quint16>& nameMap);
+bool parseRecord(const QByteArray& packet, quint16& offset, Record& record);
+void writeRecord(QByteArray& packet, quint16& offset, Record& record, QMap<QByteArray, quint16>& nameMap);
+bool fromPacket(const QByteArray& packet, Message& message);
+void toPacket(const Message& message, QByteArray& packet);
+QString typeName(quint16 type);
+QTNETWORKNG_NAMESPACE_END
+#endif // DNS_H
---
+++ include/dnsserver.h
@@ -0,0 +1,130 @@
+﻿#ifndef DNSSERVER_H
+#define DNSSERVER_H
+#include <QObject>
+#include "dns.h"
+QTNETWORKNG_NAMESPACE_BEGIN
+class AbstractServer : public QObject {
+	Q_OBJECT
+public:
+	explicit AbstractServer(QObject* parent = 0);
+	virtual void sendMessage(const Message& message) = 0;
+	virtual void sendMessageToAll(const Message& message) = 0;
+Q_SIGNALS:
+	void messageReceived(const Message& message);
+	void error(const QString& message);
+};
+class CachePrivate;
+class Cache : public QObject {
+	Q_OBJECT
+public:
+	explicit Cache(QObject* parent = 0);
+	void addRecord(const Record& record);
+	bool lookupRecord(const QByteArray& name, quint16 type, Record& record) const;
+	bool lookupRecords(const QByteArray& name, quint16 type, QList<Record>& records) const;
+Q_SIGNALS:
+	void shouldQuery(const Record& record);
+	void recordExpired(const Record& record);
+private:
+	CachePrivate* const d;
+};
+class HostNamePrivate;
+class HostName : public QObject {
+	Q_OBJECT
+public:
+	HostName(AbstractServer* server, QObject* parent = 0);
+	~HostName();
+	bool isRegistered() const;
+	QByteArray hostname() const;
+        static QString getLocalHostName();
+Q_SIGNALS:
+	void hostnameChanged(const QByteArray& hostname);
+private:
+	HostNamePrivate* const d;
+};
+class ServicePrivate;
+class Service
+{
+public:
+	Service();
+	Service(const Service& other);
+	Service& operator=(const Service& other);
+	bool operator==(const Service& other) const;
+	bool operator!=(const Service& other) const;
+	virtual ~Service();
+	void setAddress(const HostAddress& address);
+	HostAddress address() const;
+	QByteArray type() const;
+	void setType(const QByteArray& type);
+	QByteArray name() const;
+	void setName(const QByteArray& name);
+	QByteArray hostname()const;
+	void setHostname(const QByteArray& hostname);
+	quint16 port() const;
+	void setPort(quint16 port);
+	QMap<QByteArray, QByteArray> attributes() const;
+	void setAttributes(const QMap<QByteArray, QByteArray>& attributes);
+	void addAttribute(const QByteArray& key, const QByteArray& value);
+private:
+	ServicePrivate* const d;
+};
+
+class ProberPrivate;
+class Prober : public QObject
+{
+	Q_OBJECT
+public:
+	Prober(AbstractServer* server, const Record& record, QObject* parent = 0);
+Q_SIGNALS:
+	void nameConfirmed(const QByteArray& name);
+private:
+	ProberPrivate* const d;
+};
+class ProviderPrivate;
+class Provider :public QObject {
+	Q_OBJECT
+public:
+	Provider(AbstractServer* server, HostName* hostname, QObject* parent = 0);
+	void update(const Service& service);
+private:
+	ProviderPrivate* const d;
+};
+class ResolverPrivate;
+class Resolver : public QObject
+{
+	Q_OBJECT
+public:
+	Resolver(AbstractServer* server, const QByteArray& name, Cache* cache = 0, QObject* parent = 0);
+
+Q_SIGNALS:
+	void resolved(const HostAddress& address);
+private:
+	ResolverPrivate* const d;
+};
+class DnsServerPrivate;
+class DnsServer : public AbstractServer
+{
+	Q_OBJECT
+public:
+	explicit DnsServer(QObject* parent = 0);
+	virtual void sendMessage(const Message& message);
+	virtual void sendMessageToAll(const Message& message);
+private:
+	DnsServerPrivate* const d;
+};
+class BrowserPrivate;
+class Browser : public QObject
+{
+	Q_OBJECT
+public:
+	Browser(AbstractServer* server, const QByteArray& type, Cache* cache = 0, QObject* parent = 0);
+	QMap<QByteArray, Service> getServices();
+	void sendQueryAll();
+Q_SIGNALS:
+	void serviceAdded(const Service& service);
+	void serviceUpdated(const Service& service);
+	void serviceRemoved(const Service& service);
+private:
+	BrowserPrivate* const d;
+};
+QTNETWORKNG_NAMESPACE_END
+#endif // DNSSERVER_H
